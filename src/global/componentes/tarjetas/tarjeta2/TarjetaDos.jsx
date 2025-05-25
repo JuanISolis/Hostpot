5c9106a12b19feaac1606380e991b1853afd64dd
cristian mosquera <cmosqbone1@gmail.com>
2025-05-25 20:52:18 +0200

    agregar cambios

--- src/global/componentes/tarjetas/tarjeta2/TarjetaDos.jsx
+++ src/global/componentes/tarjetas/tarjeta2/TarjetaDos.jsx
@@ -2,21 +2,20 @@ import React from 'react'
 import PropTypes from 'prop-types';
 
 export const TarjetaDos = ({
-    imagen,
-    ubicacion,
-    direccion,
+  ubicacion,
+  direccion,
 }) => {
   return (
     <div>
-        <img src={imagen} alt="imagen de tarjeta"  />
-        <p><strong>{ubicacion}</strong></p>
-        <p>{direccion}</p>
+
+      <p><strong>{ubicacion}</strong></p>
+      <p>{direccion}</p>
     </div>
   )
 }
 
 TarjetaDos.propTypes = {
-    imagen: PropTypes.string,
-    ubicacion: PropTypes.string,
-    direccion: PropTypes.string,
-  };
+  imagen: PropTypes.string,
+  ubicacion: PropTypes.string,
+  direccion: PropTypes.string,
+};
