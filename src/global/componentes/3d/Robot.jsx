7db4de367e99c1ae7e6fb991d436ea605f0c6878
cristian mosquera <cmosqbone1@gmail.com>
2025-05-28 21:38:52 +0200

    carrousel

--- src/global/componentes/3d/Robot.jsx
+++ src/global/componentes/3d/Robot.jsx
@@ -1,24 +1,21 @@
-import Spline from '@splinetool/react-spline';
-import style from './robot.module.css';
-import { useRef } from 'react';
+import Spline from '@splinetool/react-spline'
+import style from './robot.module.css'
+import { useRef } from 'react'
 
 export const Robot = () => {
-
-
-    const robotRef = useRef(null);
+    const robotRef = useRef(null)
 
     const handleClick = () => {
-        const robot = robotRef.current;
+        const robot = robotRef.current
         if (robot) {
-            robot.classList.add('jump');
-            setTimeout(() => robot.classList.remove('jump'), 600); // duración de la animación
+            robot.classList.add(style.jump)
+            setTimeout(() => robot.classList.remove(style.jump), 600)
         }
-    };
-    return (<div className={style.Robot}
-        ref={robotRef}
-        onClick={handleClick}
-    >
-        <Spline scene="https://prod.spline.design/1ExdN7C30UAhpf0d/scene.splinecode" />
-    </div>
+    }
+
+    return (
+        <div className={style.Robot} ref={robotRef} onClick={handleClick}>
+            <Spline scene="https://prod.spline.design/1ExdN7C30UAhpf0d/scene.splinecode" />
+        </div>
     )
 }
