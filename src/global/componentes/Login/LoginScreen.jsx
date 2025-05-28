4d6ddbdb87117edb246330f1b5d66664c586ac62
cristian mosquera <cmosqbone1@gmail.com>
2025-05-28 21:33:18 +0200

    agregando

--- src/global/componentes/Login/LoginScreen.jsx
+++ src/global/componentes/Login/LoginScreen.jsx
@@ -4,12 +4,10 @@ import style from "./login.module.css"
 
 import { AiFillBackward } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import Carousel from "../carrousel/Carrousel";
 export const LoginScreen = () => {
     const [stateformPin, setStateformPin] = useState(true)
-
-
     const navigate = useNavigate()
-
     const handleBtnBack = () => {
         navigate('/')
     }
@@ -22,7 +20,6 @@ export const LoginScreen = () => {
 
             const interval = setInterval(() => {
                 setDisplayedText((prev) => {
-                    // Agregamos la siguiente letra basada en el índice actual
                     const nextChar = text.charAt(index);
                     index++;
                     return prev + nextChar;
@@ -40,54 +37,60 @@ export const LoginScreen = () => {
     };
 
     return (
-        <div className={style.loginScreen}>
 
-            <div className={style.containerBtnBackLogin}>
-                <button className={style.returnBtn} onClick={handleBtnBack}>
-                    <AiFillBackward />
-                    Regresar
-                </button>
-            </div>
-            <div className={style.login}>
-                <h3>COFIBER</h3>
-                <TypingEffect text="Conectividad sin limites" />
-                <div className={style.chooseUserPin}>
-                    <button
-                        className={`${style.chooseUserPinBtn} ${!stateformPin ? style.active : ''}`}
-                        onClick={() => setStateformPin(false)}
-                    >
-                        Pin
-                    </button>
+        <>
 
-                    <button
-                        className={`${style.chooseUserPinBtn} ${stateformPin ? style.active : ''}`}
-                        onClick={() => setStateformPin(true)}
-                    >
-                        User
+
+            <div className={style.loginScreen}>
+                <div className={style.containerBtnBackLogin}>
+                    <button className={style.returnBtn} onClick={handleBtnBack}>
+                        <AiFillBackward />
+                        Regresar
                     </button>
                 </div>
-                <form >
-                    {stateformPin ?
-                        (
-                            <div className={style.userBox}>
-                                <input type="text" id="user" name="user" placeholder="ingrese su nombre del usuario" />
-                                <input type="text" id="password" name="password" placeholder="ingrese su password" />
-                            </div>
-
-                        )
-                        : (
-
-                            <div className={style.pinBox}>
-                                <input type="text" id="pin" name="pin" placeholder=" ingrese porfavor su pin" />
-                            </div>
-                        )}
-
+                <div className={style.login}>
+                    <h3>COFIBER</h3>
+                    <TypingEffect text="Conectividad sin limites" />
                     <div className={style.chooseUserPin}>
-                        <button className={style.chooseUserPinBtn}>Conectarse</button>
-                        <button className={style.chooseUserPinBtn}>Prueba Gratis</button>
+                        <button
+                            className={`${style.chooseUserPinBtn} ${!stateformPin ? style.active : ''}`}
+                            onClick={() => setStateformPin(false)}
+                        >
+                            Pin
+                        </button>
+
+                        <button
+                            className={`${style.chooseUserPinBtn} ${stateformPin ? style.active : ''}`}
+                            onClick={() => setStateformPin(true)}
+                        >
+                            User
+                        </button>
                     </div>
-                </form>
+                    <form >
+                        {stateformPin ?
+                            (
+                                <div className={style.userBox}>
+                                    <input type="text" id="user" name="user" placeholder="ingrese su nombre del usuario" />
+                                    <input type="text" id="password" name="password" placeholder="ingrese su password" />
+                                </div>
+
+                            )
+                            : (
+
+                                <div className={style.pinBox}>
+                                    <input type="text" id="pin" name="pin" placeholder=" ingrese porfavor su pin" />
+                                </div>
+                            )}
+
+                        <div className={style.chooseUserPin}>
+                            <button className={style.chooseUserPinBtn}>Conectarse</button>
+                            <button className={style.chooseUserPinBtn}>Prueba Gratis</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+            <Carousel />
+
+        </>
     )
 }
