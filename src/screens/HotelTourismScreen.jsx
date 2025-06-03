fddfbf6036eabb46f549ba7590dc92d5495a80fd
cristian mosquera <cmosqbone1@gmail.com>
2025-06-03 14:57:03 +0200

    agregando descripciones

--- src/screens/HotelTourismScreen.jsx
+++ src/screens/HotelTourismScreen.jsx
@@ -11,12 +11,18 @@ export const HotelTourismScreen = () => {
       <AnimatedHero />
       <div className={style.cardHotel}>
         <img src={imgHotel1} alt="imagen de hotel 1" />
+        <p>shushufindi central</p>
+        <p>avenida unidad nacional y xxx</p>
       </div>
       <div className={style.cardHotel}>
         <img src={imgHotel2} alt="imagen de hotel 2" />
+        <p>shushufindi , Barrio Eloy Alfaro </p>
+        <p>Calle Siona Y Calle 3B</p>
       </div>
       <div className={style.cardHotel}>
         <img src={imgHotel3} alt="imagen de hotel 3" />
+        <p>shushufindi, 7 DE junio</p>
+        <p>frente al control Policial</p>
       </div>
       <Footer />
     </div>
