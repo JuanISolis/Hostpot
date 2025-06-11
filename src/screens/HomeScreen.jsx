bc2e8dca4ab3cd4fc1acd8c6800ce2d6654662ae
cristian mosquera <cmosqbone1@gmail.com>
2025-06-11 20:32:17 +0200

    asd

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,8 @@
 
 
 import { Robot } from '../componentes/3d/Robot'
+import { CarrouselSwiper } from '../componentes/carrousel/CarrouselSwiper'
+import { CarrouselSwiperHOME } from '../componentes/CarrouselSwiperHOME/CarrouselSwiperHOME'
 import { Footer } from '../componentes/Footer'
 
 
@@ -8,6 +10,7 @@ export const HomeScreen = () => {
     return (
         <>
             <Robot />
+            <CarrouselSwiperHOME />
             <Footer />
         </>
     )
