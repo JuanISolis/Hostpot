b2dc2709fb95875ec7ec33c65fd77113fdc3424d
cristian mosquera <cmosqbone1@gmail.com>
2025-07-09 21:30:26 +0200

    agregando video

---
+++ src/componentes/backgroundVideo/BackgroundVideo.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Styles from "./BackgroundVideo.module.css";
+
+import video from "../../assetsCris/video/WhatsApp Video 2025-07-04 at 02.01.50.mp4";
+import { ButtonLogin } from '../ButtonLogin/ButtonLogin';
+
+
+const BackgroundVideo = () => {
+    return (
+        <div className={Styles.videocontainer}>
+            <video
+                autoPlay
+                loop
+            // muted
+            // playsInline
+            >
+                <source src={video} type="video/mp4" />
+
+            </video>
+            <ButtonLogin title="Login" />
+        </div>
+    );
+};
+
+export default BackgroundVideo;
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,17 @@
 
 
 import { Robot } from '../componentes/3d/Robot'
+import BackgroundVideo from '../componentes/backgroundVideo/BackgroundVideo'
 import { Footer } from '../componentes/Footer'
+import styles from './HomeScreen.module.css'
 
 
 export const HomeScreen = () => {
     return (
-        <>
-            <Robot />
+        <div className={styles.homescreen} >
+            {/* <Robot /> */}
+            <BackgroundVideo />
             <Footer />
-        </>
+        </div>
     )
 }
